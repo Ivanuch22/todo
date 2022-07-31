43a0e559f019b2ad8e09fe353a5fd90ae12544cc
Ivan <ivan.kalunuch12@gmail.com>
2022-08-01 02:11:20 +0300

    Create a function"addNewTask"

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,17 +1,23 @@
 const input = document.querySelector('.block-input')
 const button = document.querySelector('button')
+const block = document.querySelector('.block-top')
 
-const addNewThing = (text) => {
+const addNewTask = (text) => {
     const blockLine = document.createElement('div');
     blockLine.classList.add('block-line');
-    blockLine.innerHTML = ``
+    blockLine.innerHTML = `
+    <div class="block-checkbox">
+        <input checked type="checkbox">
+    </div>
+    <p class="block-title">${text} </p>
+    `
+    block.append(blockLine)
 }
-
-
 
 button.addEventListener("click", (e) => {
     e.preventDefault();
-    addNewThing(input.value)
+    addNewTask(input.value)
+    input.value = ''
 })
 
 //потрібно зробити функціонал для добавлення завдання в список де текст буде братись з input-а
