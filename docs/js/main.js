22a14f31e2c2b5e0398d513da6b5ba4bd8345f21
Ivan <ivan.kalunuch12@gmail.com>
2022-08-02 01:45:41 +0300

    fix bugs

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,41 +1,32 @@
-
 const input = document.querySelector('.block-input');
 const button = document.querySelector('button');
 const block = document.querySelector('.block-top');
 let id = 0;
 let arrayGlobal = [];
 
-//////
 button.addEventListener("click", (e) => {
     e.preventDefault();
     if (input.value) {
         id++
-        addNewTask(input.value, id);
-        setLocalStorageInput(input.value, id)
-        chageLocalValue()
+        setLocalStorageInput(input.value, id, false)
+        addNewTask(input.value, false);
         input.value = '';
     }
-
 });
 
-addNewTask = (text, id = 0, value = false) => {
+addNewTask = (text, value) => {
     const blockLine = document.createElement('div');
     blockLine.classList.add('block-line');
-
     blockLine.innerHTML = `
     <div class="block-checkbox">
-        ${value ? `<input  type="checkbox" checked id = "${id}">` : `<input  type="checkbox" id = "${id}">`}
+    ${value ? `<input  type="checkbox" checked >` : `<input  type="checkbox" >`}
     </div>
     <p class="block-title">${text} </p>
     `;
     block.append(blockLine);
 
-
-
 };
-
-
-setLocalStorageInput = (text, id, boolean = false) => {
+setLocalStorageInput = (text, id, boolean) => {
     arrayGlobal.push({ name: text, id: id, value: boolean })
     localStorage.setItem('task', JSON.stringify(arrayGlobal));
 }
@@ -50,44 +41,33 @@ getLocalStorageInput = () => {
         arrayGlobal.push(...array);
         // відображення tasks
         arrayGlobal.forEach(e => {
-            const { name, id, value } = e;
-            addNewTask(name, id, value)
+            const { name, value } = e;
+            addNewTask(name, value)
         })
     } else {
         arrayGlobal = [];
     };
-    return array;
 };
-getLocalStorageInput();
+getLocalStorageInput()
 
-
-chageLocalValue = () => {
-    const checkboxs = document.querySelectorAll('input[type= checkbox]');
-    checkboxs.forEach((element, id) => {
-        element.addEventListener("click", (e) => {
-            console.log("Hello")
-            if (e.target.checked) {
-                changeValue(id)
-            } else {
-                changeValue(id)
-
-            }
-        })
+block.addEventListener('click', (e) => {
+    const child = document.querySelectorAll('input[type=checkbox]')
+    child.forEach((element, id) => {
+        if (e.target === element) {
+            changeValue(id)
+        }
     })
-    changeValue = (id) => {
-        let array = localStorage.getItem('task');
-        array = JSON.parse(array);
-        array[id].value = !array[id].value;
-        localStorage.setItem("task", JSON.stringify(array))
+})
+
+changeValue = (id) => {
+    let array = localStorage.getItem('task');
+    array = JSON.parse(array);
+    if (array[id].value) {
+        array[id].value = false;
+    } else {
+        array[id].value = true;
     }
-
+    localStorage.setItem("task", JSON.stringify(array));
 }
-chageLocalValue()
-
-
-
-
-
-
 
 
